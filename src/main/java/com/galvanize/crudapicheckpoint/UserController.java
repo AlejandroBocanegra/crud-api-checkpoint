e303ca8fed6cace0de398fea2c018d35b13fef29
AlejandroBocanegra <a.boca@outlook.com>
2020-07-24 02:07:47 +0900

    lunch push

--- src/main/java/com/galvanize/crudapicheckpoint/UserController.java
+++ src/main/java/com/galvanize/crudapicheckpoint/UserController.java
@@ -3,6 +3,7 @@ package com.galvanize.crudapicheckpoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +22,21 @@ public class UserController {
     public User create(@RequestBody User user) {
         return this.repository.save(user);
     }
+
+    @GetMapping("/users/{id}")
+    public User buscaById(@RequestParam("id") Long id){
+        return this.repository.findPorUserId(id);
+    }
+
+    // @GetMapping("/users/{id}")
+    // public User eliminarById(@RequestParam("id") Long id){
+    //     return this.repository.deletePorId(id);
+    // }
+
+    // @GetMapping("/users/{id}")
+    // public User actualizarById(@RequestParam("email") String email){
+    //     return this.repository.updatePorEmail(email);
+    // }
+
+
 }
--- src/main/java/com/galvanize/crudapicheckpoint/UserRepository.java
+++ src/main/java/com/galvanize/crudapicheckpoint/UserRepository.java
@@ -1,7 +1,17 @@
 package com.galvanize.crudapicheckpoint;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     
+    @Query(value= "SELECT * FROM users WHERE id = :id", nativeQuery=true)
+    User findPorUserId(Long id);
+
+    @Query(value= "DELETE * FROM users WHERE id = :id", nativeQuery=true)
+    User deletePorId(Long id);
+
+    @Query(value= "REPLACE INTO users SET id = :id, email = :email", nativeQuery=true)
+    User updatePorEmail(String email);
+
 }
